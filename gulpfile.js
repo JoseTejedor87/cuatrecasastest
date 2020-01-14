28849d610d406ebf11dc7e40d978995ab0df9685
Eva Fernández Osorio <EFEO@cuatrecasas.com>
2020-01-14 17:36:03 +0100

    Archivos gulp para compilar SASS

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
+
+
+// File paths
+const files = {
+    scssPath: 'templates/web/src/scss/**/*.scss',
+    jsPath: 'templates/websrc/js/**/*.js'
+}
+
+// Sass task: compiles the style.scss file into style.css
+function scssTask(){
+    return src(files.scssPath)
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('public/web/assets/css')
+    ); // put final CSS in dist folder
+}
+
+// JS task: concatenates and uglifies JS files to script.js
+function jsTask(){
+    return src([
+        files.jsPath
+        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+        ])
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(dest('public/web/assets/js')
+    );
+}
+
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
+function watchTask(){
+    watch([files.scssPath, files.jsPath],
+        series(
+            parallel(scssTask, jsTask)
+        )
+    );
+}
+
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then watch task
+exports.default = series(
+    parallel(scssTask, jsTask),
+    watchTask
+);
