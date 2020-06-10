b1fd6a75dd8887225069be45696639f676279f63
Javier Marrero <jmarrerom@etics.es>
2020-06-10 18:42:28 +0200

    Mejoras en NavigationService
    Se han incluido los parámetros opcionales language y region en getPathByPublishable para obtener, cuando sea necesario, el path del Publishable relativo a estos parámetros. Útil especialmente para la definición de los hreflang del header y los selectores de idioma y región del menú.
    
    Se ha incluido getHrefLang para obtener el codigo alternativo iso válido para los header link rel=alternate
    
    Se ha incluido getAlternativePath para facilitar la obtención se las rutas alternativas del documento en curso para los diferentes idiomas y regiones

--- src/Controller/Web/NavigationService.php
+++ src/Controller/Web/NavigationService.php
@@ -27,53 +27,100 @@ class NavigationService
         return $params['_region'];
     }
 
-    public function getPathByPublishable($publishable)
+    public function getHrefLang($language, $region)
     {
-        $path = '';
-        if ($publishable instanceof \App\Entity\Article) {
-            $path = $this->router->generate('articles_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Desk) {
-            $path = $this->router->generate('desks_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Event) {
-            $path = $this->router->generate('events_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Insight) {
-            $path = $this->router->generate('insights_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Lawyer) {
-            $path = $this->router->generate('lawyers_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Office) {
-            $path = $this->router->generate('offices_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Page) {
-            $path = $this->router->generate('pages_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Practice) {
-            $path = $this->router->generate('practices_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Product) {
-            $path = $this->router->generate('products_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Resource) {
-            $path = $this->router->generate('resources_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
-        } elseif ($publishable instanceof \App\Entity\Sector) {
-            $path = $this->router->generate('sectors_detail', [
-                'slug' => $publishable->translate()->getSlug()
-            ]);
+        $hreflangs = [];
+        $hreflangs['es']['spain'] = 'es-es';
+        $hreflangs['es']['latam'] = 'es';
+        $hreflangs['en']['spain'] = 'en-es';
+        $hreflangs['en']['latam'] = 'en';
+
+        return $hreflangs[$language][$region] ?? '';
+    }
+
+    public function getAlternativePath($language = null, $region = null)
+    {
+        $language = $language ?? $this->getLanguage();
+        $region = $region ?? $this->getRegion();
+
+        // if there is a publishable instance in the current request,
+        // use "getPathByPublishable" to obtain the custom path
+        // defined for the current published instance. In other case
+        // just rewrite the current request using the $language and
+        // $region parameters
+
+        if ($publishable = $this->router->getContext()->getParameter('_publishable')) {
+            // Calling getPathByPublishable to retrieve the custom path
+            // for the current $publishable instance
+            $path = $this->getPathByPublishable($publishable, $language, $region);
+        } else {
+            // Rewriting the current request using
+            // the $language and $region parameters
+            $pathName = $this->request->get('_route');
+            $path = $this->router->generate(
+                $pathName,
+                [
+                    '_locale' => $language,
+                    '_region' => $region
+                ],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+        }
+        return $path;
+    }
+
+    // Obtain the custom path for an specific publishable instance.
+    // The optional parameters $language and $region could be used to force some values.
+    // If no optional parameter received, the method use the values from the current request.
+    public function getPathByPublishable($publishable, $language = null, $region = null)
+    {
+        $path = null;
+        $params = [];
+
+        $language = $language ?? $this->router->getContext()->getParameter('_locale');
+        $region = $region ?? $this->router->getContext()->getParameter('_region');
+
+        $params['_locale'] = $language;
+        $params['_region'] = $region;
+
+        // To avoid the path calculation of a not publishable instances and unpublished instances
+        if (($publishable instanceof \App\Entity\Publishable) && $publishable->isPublished($language, $region)) {
+            if ($publishable instanceof \App\Entity\Article) {
+                $pathName = 'articles_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Desk) {
+                $pathName = 'desks_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Event) {
+                $pathName = 'events_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Insight) {
+                $pathName = 'insights_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Lawyer) {
+                $pathName = 'lawyers_detail';
+                $params['slug'] = $publishable->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Office) {
+                $pathName = 'offices_detail';
+                $params['slug'] = $publishable->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Page) {
+                $pathName = 'pages_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Practice) {
+                $pathName = 'practices_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Product) {
+                $pathName = 'products_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Resource) {
+                $pathName = 'resources_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            } elseif ($publishable instanceof \App\Entity\Sector) {
+                $pathName = 'sectors_detail';
+                $params['slug'] = $publishable->translate($language)->getSlug();
+            }
+
+            $path = $this->router->generate($pathName, $params, UrlGeneratorInterface::ABSOLUTE_URL);
         }
 
         return $path;
