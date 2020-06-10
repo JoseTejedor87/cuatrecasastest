e87a01aafeaa225a1a199ef8338de2b5c962bcdc
Javier Marrero <jmarrerom@etics.es>
2020-06-10 10:42:14 +0200

    Añadido slug, regions y summary a ActivityForm

--- src/Form/ActivityFormType.php
+++ src/Form/ActivityFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 use App\Form\Type\LanguageType;
+use App\Form\Type\RegionType;
 
 abstract class ActivityFormType extends AbstractType
 {
@@ -20,6 +21,8 @@ abstract class ActivityFormType extends AbstractType
             ->add('translations', TranslationsType::class, [
                 'fields' => [
                     'title' => ['label'=>'entities.activity.fields.title', 'required'=>true],
+                    'slug' => ['label'=>'entities.activity.fields.slug'],
+                    'summary' => ['label'=>'entities.activity.fields.summary', 'attr'=>['class'=>'summernote']],
                     'description' => ['label'=>'entities.activity.fields.description', 'attr'=>['class'=>'summernote']],
                     'metaTitle' => ['label'=>'entities.publishable.fields.metaTitle'],
                     'metaDescription' => ['label'=>'entities.publishable.fields.metaDescription']
@@ -27,6 +30,7 @@ abstract class ActivityFormType extends AbstractType
             ])
             ->add('highlighted', CheckboxType::class, ['label'=>'entities.activity.fields.highlighted'])
             ->add('languages', LanguageType::class, ['label'=>'entities.publishable.fields.languages'])
+            ->add('regions', RegionType::class, ['label'=>'entities.publishable.fields.regions'])
             ->add('image', TextType::class, ['label'=>'entities.activity.fields.image']);
     }
 
