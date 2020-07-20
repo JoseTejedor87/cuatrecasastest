051a80f9a3d747630d3cca091e5efe5ab45d12fd
Jose Tejedor Bello <JTEB@cuatrecasas.com>
2020-07-20 09:47:19 +0200

    insights

--- src/Controller/Web/InsightController.php
+++ src/Controller/Web/InsightController.php
@@ -6,13 +6,47 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Repository\InsightRepository;
 use App\Repository\CaseStudyRepository;
 use App\Controller\Web\WebController;
+use App\Repository\PublicationRepository;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 
 class InsightController extends WebController
 {
-    public function detail(Request $request, InsightRepository $insightRepository, CaseStudyRepository $caseStudyRepository)
+    protected $imagineCacheManager;
+
+    public function __construct(CacheManager $imagineCacheManager)
+    {
+     
+        $this->imagineCacheManager = $imagineCacheManager;
+    }
+    public function detail(Request $request, InsightRepository $insightRepository, CaseStudyRepository $caseStudyRepository, PublicationRepository $publicationRepository)
     {
         $insight = $insightRepository->getInstanceByRequest($request);
-
+        $limit = 14;
+        $page = $request->query->get('page') ?: 1;
+        $query = $publicationRepository->createQueryBuilder('p');
+        $query = $query->andWhere('p.publication_date < :day')->setParameter('day', date("Y-m-d"))->orderBy('p.publication_date', 'DESC');
+        $countPublications = count($query->getQuery()->getResult());
+        $query = $query->setFirstResult($limit * ($page - 1))
+                ->setMaxResults($limit)
+                ->getQuery();
+        $publications = $query->getResult();
+        foreach ($publications as $key => $value) {
+            $value->fechaPubli = $value->getPublicationDate()->format("j F Y");
+            if ($value instanceof \App\Entity\LegalNovelty || $value instanceof \App\Entity\Article || $value instanceof \App\Entity\Research){
+                $value->type = 'article';
+            }
+            if ($value instanceof \App\Entity\Opinion){
+                $value->type = 'opinion';
+            }
+            if ($value instanceof \App\Entity\News){
+                $value->type = 'news';
+            }
+            $value->photo = $this->getPhotoPathByFilter($value, 'lawyers_grid');
+            if(!$value->photo){
+                $value->photo = 'https://via.placeholder.com/800x400';
+            }
+            
+        }
         // TODO:
         // Revisar $insight y rellenar $contextualBlocks con las diferentes collecciones
         // de elementos en función del estado de los atributos $showKnowledgeBlock, $showEventsBlock,
@@ -26,7 +60,24 @@ class InsightController extends WebController
 
         return $this->render('web/insights/detail.html.twig', [
             'insight' => $insight,
-            'contextualBlocks' => $contextualBlocks
+            'contextualBlocks' => $contextualBlocks,
+            'publications' => $publications,
         ]);
+    }
+    protected function getPhotoPathByFilter($publication, $filter)
+    {
+        if ($photos = $publication->getAttachments()) {
+            foreach ($photos as $key => $photo) {
+
+                if($photo->getType() == "publication_main_photo"){
+                    $photo = $this->imagineCacheManager->getBrowserPath(
+                        '/resources/' . $photo->getFileName(),
+                        $filter
+                    );
+                    return $photo;
+                }  
+            }
+        }
+        
     }
 }
