b66bf0bff1ff58c67eb87983b8f7e08eb8a58be6
Jose Tejedor Bello <JTEB@cuatrecasas.com>
2020-10-31 22:17:04 +0100

    Publication API & publication-pages(n:n)

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -865,11 +865,11 @@ class ImportCommand extends Command
     {
         $this->em->getConnection()->executeQuery("DELETE FROM Page ");
         // $this->em->getConnection()->executeQuery("ALTER TABLE Page AUTO_INCREMENT = 1");
-        $this->em->getConnection()->executeQuery("DBCC CHECKIDENT ([Page], RESEED, 1)");
+        // $this->em->getConnection()->executeQuery("DBCC CHECKIDENT ([Page], RESEED, 1)");
 
         $this->em->getConnection()->executeQuery("DELETE FROM PageTranslation ");
         // $this->em->getConnection()->executeQuery("ALTER TABLE PageTranslation AUTO_INCREMENT = 1");
-        $this->em->getConnection()->executeQuery("DBCC CHECKIDENT ([PageTranslation], RESEED, 1)");
+        // $this->em->getConnection()->executeQuery("DBCC CHECKIDENT ([PageTranslation], RESEED, 1)");
         $data = file_get_contents("JsonExports/pages.json");
         $items = json_decode($data, true);
 
